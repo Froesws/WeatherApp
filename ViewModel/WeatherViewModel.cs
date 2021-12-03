0d7596a2b28e5e7e5e912217ac3b426515867e94
William Froes <froes.ws@gmail.com>
2021-12-02 21:50:05 -0300

    Using Design Mode Bindings, created ViewModel Class

---
+++ ViewModel/WeatherViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string query;
+
+        public string Query
+        {
+            get
+            {
+                return query;
+            }
+            set
+            {
+                query = value;
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+        private CurrentConditions currentConditions;
+        public CurrentConditions CurrentConditions
+        {
+            get
+            {
+                return currentConditions;
+            }
+            set
+            {
+                currentConditions = value;
+                OnPropertyChanged(nameof(CurrentConditions));
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get
+            {
+                return selectedCity;
+            }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged(nameof(SelectedCity));
+            }
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public WeatherViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly Cloudly",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 22
+                        }
+                    }
+                };
+            }
+        }
+
+    }
+}
